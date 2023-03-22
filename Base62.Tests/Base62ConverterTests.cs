c10e539081575a0e00804b023d1f98a3c0c6cb35
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2023-03-22 00:53:19 -0500

    feat: initial commit

---
+++ Base62.Tests/Base62ConverterTests.cs
@@ -0,0 +1,63 @@
+namespace Base62.Tests
+{
+    [TestClass]
+    public class Base62ConverterTests
+    {
+        [DataTestMethod]
+        [DataRow(0, "0")]
+        [DataRow(7, "7")]
+        [DataRow(16, "g")]
+        [DataRow(61, "Z")]
+        [DataRow(65, "13")]
+        [DataRow(999, "g7")]
+        [DataRow(9999, "2Bh")]
+        [DataRow(238327, "ZZZ")]
+        [DataRow(2009215674938, "zn9edcu")]
+        [DataRow(23423234234234, "6EnvxMIa")]
+        [DataRow(377794100552163282, "rUiF01cmr0")]
+        public void Encode_Is_Correct(long id, string expected)
+        {
+            var encoded = Base62Converter.Encode(id);
+
+            Assert.AreEqual(expected, encoded);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, "0")]
+        [DataRow(7, "7")]
+        [DataRow(16, "g")]
+        [DataRow(61, "Z")]
+        [DataRow(65, "13")]
+        [DataRow(999, "g7")]
+        [DataRow(9999, "2Bh")]
+        [DataRow(238327, "ZZZ")]
+        [DataRow(2009215674938, "zn9edcu")]
+        [DataRow(377794100552163282, "rUiF01cmr0")]
+        public void Decode_Is_Correct(long expected, string encoded)
+        {
+            var decoded = Base62Converter.Decode(encoded);
+
+            Assert.AreEqual(expected, decoded);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(7)]
+        [DataRow(16)]
+        [DataRow(61)]
+        [DataRow(65)]
+        [DataRow(999)]
+        [DataRow(9999)]
+        [DataRow(238327)]
+        [DataRow(2009215674938)]
+        [DataRow(23423234234234)]
+        [DataRow(377794100552163282)]
+        public void Encoded_Is_Decoded(long number)
+        {
+            var encoded = Base62Converter.Encode(number);
+            var decoded = Base62Converter.Decode(encoded);
+
+            Assert.AreEqual(number, decoded);
+        }
+    }
+}
---
+++ Base62/Base62Converter.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace Base62
+{
+    public static class Base62Converter
+    {
+        private const string BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        /// <summary>
+        /// Encode the long <see cref="value"/> to a Base62 string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Encode(long value)
+        {
+            return Encode(value, BASE62);
+        }
+
+        /// <summary>
+        /// Decode the Base62 string <see cref="value"/> to a long number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static long Decode(string value)
+        {
+            return Decode(value, BASE62);
+        }
+
+        private static string Encode(long value, string table)
+        {
+            var baseLength = table.Length;
+
+            StringBuilder sb = new();
+
+            var reminder = (int)(value % baseLength);
+
+            sb.Insert(0, table[reminder]);
+
+            var q = Math.Floor((double)value / baseLength);
+            while (q > 0)
+            {
+                reminder = (int)(q % baseLength);
+
+                sb.Insert(0, table[reminder]);
+
+                q = Math.Floor(q / baseLength);
+            }
+
+            return sb.ToString();
+        }
+
+        private static long Decode(string value, string table)
+        {
+            var baseLength = table.Length;
+
+            long result = 0;
+
+            for(var i = 0; i < value.Length; i++)
+            {
+                var position = value.Length - i - 1;
+
+                result += table.IndexOf(value[i]) * (long)Math.Pow(baseLength, position);
+            }
+
+            return result;
+        }
+    }
+}
